e804c76155b8868c8b4a2c01c93f7d4d20d7af4e
Bartosz Celuch <bartek.celuch@gmail.com>
2022-01-31 20:12:14 +0100

    add get to week

--- src/main/java/com/example/gosiabartekroadtoweeding/Week/WeekEntityService.java
+++ src/main/java/com/example/gosiabartekroadtoweeding/Week/WeekEntityService.java
@@ -1,7 +1,9 @@
 package com.example.gosiabartekroadtoweeding.Week;
 
 import com.example.gosiabartekroadtoweeding.DayOfEating.DayOfEatingService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class WeekEntityService {
@@ -14,6 +16,20 @@ public class WeekEntityService {
     }
 
     public WeekEntity addNewWeek(Long dayId, int weeklyCaloriesIntake) {
-           return weekEntityRepository.save(new WeekEntity(dayOfEatingService.saveAllWeek(dayId), weeklyCaloriesIntake));
+        return weekEntityRepository.save(new WeekEntity(dayOfEatingService.saveAllWeek(dayId), weeklyCaloriesIntake));
+    }
+
+    public WeekData getWeekByDayId(Long dayId) {
+        var week = weekEntityRepository.findAll().stream()
+                .filter(weekEntity -> weekEntity.getDays().stream().anyMatch(day -> day.getId().equals(dayId))).findAny();
+        if (week.isPresent()) {
+            var weekData = new WeekData(week.get().getWeeklyCaloriesIntake());
+            week.get().getDays().forEach(day -> day.getMeals()
+                    .forEach(mealEntity -> weekData.setCurrentCalories(weekData.getCurrentCalories() + mealEntity.getCalories())));
+            return weekData;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "There is no week with that dayId: " + dayId + "in it");
+        }
     }
 }
